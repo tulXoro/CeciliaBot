3614199b1380da49d3e3b93dd97a5a4ee710b9ec
Tim <43763078+tulXoro@users.noreply.github.com>
2019-09-07 22:19:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const Discord = require('discord.js');//dependency
+const YTDL = require('ytdl-core');
+
+//const fs = require('fs');
 const NAME = 'Cecilia';//Name of bot
 const VER = '1.1.1';//version # of bot
 const bot = new Discord.Client();//bot is created as a discord client
@@ -11,6 +14,8 @@ const PREFIX = '[]';
 
 //for my own discord server
 const ADMIN_ROLE = '614614883815653386';//admin role that manages the bot
+
+global.servers = {};
 
 //other vars
 var praise = 0;
@@ -94,19 +99,26 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => {
 bot.on('message', msg => {
     //if the "author" or user who sent the message is the bot, then this method stops
     if(msg.author == bot.user) return;
-
+    
     //just a simple command to praise the bot
     if(msg.content == "good bot"){
         praise++;
+        console.log(`${msg.author.username}#${msg.author.discriminator} praised me!`);
         return msg.channel.send('(.❛ ᴗ ❛.)');
+    }
+    
+    //if the bot detects a message which is in a dm or group, this method stops
+    if(!msg.guild){
+        let d = new Date();
+        let temp = `${msg.author.username}#${msg.author.discriminator} tried DMing: "${msg.content}" at` + 
+                    " " + d.getMonth() + " " + d.getDay() + " " + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes();
+        console.log(temp);
+        //log(temp);
+        return msg.author.send('Oops! Sorry I can\'t respond to direct messages!');
     }
 
     //if the message does not start with the prefix, then this method stops
     if(!msg.content.startsWith(PREFIX)) return;
-
-    //if the bot detects a message which is in a dm or group, this method stops
-    if(!msg.guild)
-        return msg.channel.sendMessage('Oops! Sorry I can\'t respond to direct messages!');
 
     //creates an array called "args" that seperates each word by a space
     //ex: "{PREFIX}Hello there!" will make an array with 2 elements
@@ -120,32 +132,73 @@ bot.on('message', msg => {
     switch(args[0]){
         //essentials
         case 'help':
-            return msg.reply('Sorry, I would if I could! But my commands are a work in progress!');
+                const embed = new Discord.RichEmbed()
+                                .setTitle('Commands')
+                                .addField('[]introduce', 'Introduce me!')
+                                .addField('[]y/n', "Ask me a yes or no question!")
+                                .addField("[]join", "This command is currently disabled! I will be able to join in the future though!")
+                                .addField('[]leave', "I'll leave the voice channel!")
+                                .addField('[]dm', "Send a direct message to someone!")
+                                .addField('[]purge', "Delete messages!")
+                                .addField('[]bot stats', "Everything you need to know about me!")
+                                .setColor(0x00FBFF)
+                                .setThumbnail(bot.user.avatarURL)
+                                .setFooter('Sorry if I\'m buggy! I am still in development! Original image: https://bit.ly/30vWVvN');
+            return channel.send(embed);
 
         //chat
         case 'introduce':
 			return channel.send("I'm a bot programmed by tulXoro (further known as Tim)! I'm currently running on his Raspberry Pi!");
         case 'apologize':
             return msg.reply('I\'m really sorry! Please accept my apology!');
+        case 'y/n':
+            if(args.length<4) return channel.send("Are you sure that's a question?");
+            let r = Math.floor(Math.random() * 2);
+            if(r==1) return channel.send("Of course!");
+            else return channel.send("No way!");
+        case 'dm':
+            let mention = msg.mentions.users.first();
+            if(!mention) return channel.send("Please mention the person you want me to DM");
+            let mentionMsg = msg.content.slice(5);
+            mention.send(`${msg.author.username}#${msg.author.discriminator} said "${mentionMsg}"`);
+            return msg.delete();
 
         //music
-        case 'join':
-            /*
-            if(member.voiceChannel){
-                member.voiceChannel.join().then(connection => {}).catch(console.log);
-                return channel.send('I\'m in the voice channel now!');
-            }return channel.send('You need to join a voice channel first!');
-            */
-           return channel.send('This feature is a WIP!');
-
         case 'leave':
             if(msg.guild.voiceConnection) {
                 msg.guild.voiceConnection.disconnect();
                 return channel.send('I left the voice channel!');
             }else return channel.send('I\'m not in a voice channel!');
+        case 'play':
+            if(member.voiceChannel){
+                if(!servers[msg.guild.id]){//when server does not exist in list
+                    servers[msg.guild.id] = { queue: [] };//add server to the list
+                }
+                member.voiceChannel.join().then(connection => {//bot joins voice channel
+                    let server = servers[msg.guild.id];//makes an instance variable of the guild
+                    server.queue.push(args[1]);//pushes url to the server queue
+                    play(connection, msg); //calls play function
+                    channel.send("I'm playing music!");
+                }).catch(console.log);
+                return;
+            }else return channel.send('You need to join a voice channel first!');
+        case 'leave':
+            if(msg.guild.voiceConnection) {
+                msg.guild.voiceConnection.disconnect();
+                return channel.send('I left the voice channel!');
+            }else return channel.send('I\'m not in a voice channel!');
+            /*
+        case 'queue':
+            var server = servers[msg.guild.id];
+            let response = "";
+            for(let i=0; i<server.queue.length; i++)
+                response+="\n" + i;
+            channel.send(response);*/
 
         //admin
-        case 'delete':
+        case 'purge':
+            if(!msg.guild.roles.has(ADMIN_ROLE))
+                return channel.end("Sorry, this command is disabled in this server at the moment!");
             if(!member.roles.has(ADMIN_ROLE))
                 return channel.send('Sorry, you don\'t have the right permissions to do that!');
             if(!args[1]) 
@@ -153,7 +206,7 @@ bot.on('message', msg => {
             if(isNaN(args[1]))
                 return channel.send('Please take this seriously! You didn\'t give me a number!');
             return channel.bulkDelete(args[1]);
-
+            
         //bot
         case 'bot':
                 if(args[1]=='stats'){
@@ -170,7 +223,6 @@ bot.on('message', msg => {
                                     .setFooter('Sorry if I\'m buggy! I am still in development! Original image: https://bit.ly/30vWVvN');
                     return channel.send(embed);
                 }
-        
     }
 })
 
@@ -185,6 +237,18 @@ function getUpTime() {
     tot_uptime = tot_uptime/24; //days
     return tot_uptime.toFixed(1) + " days";
 }
+
+function play(connection, msg){
+    let server = servers[msg.guild.id];
+    server.dipatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));//for some reason, when playing multiple, it stops
+    server.queue.shift();
+    server.dipatcher.on("end", () => {
+        if(server.queue[0]) play(connection, msg);
+        else connection.disconnect();
+    })
+}
+
+//function log(str){ fs.appendFile('log.txt', str, err => console.log(err)); }
 
 //the bot logs on and starts using the profile token
 bot.login(TOKEN);
